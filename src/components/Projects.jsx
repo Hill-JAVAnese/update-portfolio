d3680466c9c26f08a4eafbbc2e9fbdc8bc7f34d1
Hill <86607741+Hill-JAVAnese@users.noreply.github.com>
2025-07-23 00:21:03 +0700

    Update Projects.jsx
    update link to the pic to make it loaded

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ const projects = [
   {
     title: 'Sistem Diagnosa Penyakit',
     desc: 'Sistem Diagnosa Penyakit menggunakan Python dan CSV sebagai dataset.',
-    img: 'https://i.imgur.com/gXqb6XN.png',
+    img: 'https://i.imgur.com/93Db0kx.png',
     link: 'https://web-production-cccc1.up.railway.app/',
   },
   {
